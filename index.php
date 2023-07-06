6c4665208dadccbd0b8fa34cb40e80aeddd1f3f5
cdtw97 (X1 YOGA) <67127093+cdtw97@users.noreply.github.com>
2023-07-06 08:54:44 -0400

    Revamp UI and Optimize HTML Structure for Slot Machine Game
    This commit introduces several improvements to the Slot Machine game's HTML structure and design:
    
    UI Enhancements: The Bootstrap library has been introduced to make the UI more responsive and to use pre-existing Bootstrap styles. Now, the web page is easier to navigate and visually appealing with a clear display of the slot machine and the game information.
    
    Simplified Slot Machine Structure: The number of reels has been reduced to 6 from 18, and they are now placed in a single row for simplicity and improved readability. This optimizes the game's presentation and makes it more user-friendly.
    
    Winning Lines Section: A new section to display winning lines has been added, which provides immediate and clear feedback to the user after each spin.
    
    Token Display: The display of tokens has been simplified and now uses a <span> tag for dynamic updates.
    
    External Library References: The code now includes references to JQuery, Bootstrap, and GSAP (GreenSock Animation Platform) libraries. These will support enhanced interactivity, design, and animation effects in the game.
    
    These changes aim to enhance user experience and facilitate game interaction, providing a smoother and more enjoyable gaming experience.

--- index.php
+++ index.php
@@ -1,96 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Slot Machine</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div id="testing-purposes"></div>
-    <div id="slot-machine">
-        <div class="row">
-            <div class="reel-container">
-                <div class="reel" id="reel1"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel2"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel3"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel4"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel5"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel6"></div>
-            </div>
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center">
+            <h2>Simple Slot</h2>
         </div>
-        <div class="row">
-            <div class="reel-container">
-                <div class="reel" id="reel7"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel8"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel9"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel10"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel11"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel12"></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="reel-container">
-                <div class="reel" id="reel13"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel14"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel15"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel16"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel17"></div>
-            </div>
-            <div class="reel-container">
-                <div class="reel" id="reel18"></div>
-            </div>
-        </div>
-
-
-
     </div>
-    <button id="spin-button">Spin</button>
+    <div class="container">
 
+        <div class="d-flex justify-content-center align-items-center">
+        
+            <div id="slot-machine" class="d-flex justify-content-around">
+                <div class="reel-container" id="reel1">
+                    <ul class="reel"></ul>
+                </div>
+                <div class="reel-container" id="reel2">
+                    <ul class="reel"></ul>
+                </div>
+                <div class="reel-container" id="reel3">
+                    <ul class="reel"></ul>
+                </div>
+                <div class="reel-container" id="reel4">
+                    <ul class="reel"></ul>
+                </div>
+                <div class="reel-container" id="reel5">
+                    <ul class="reel"></ul>
+                </div>
+                <div class="reel-container" id="reel6">
+                    <ul class="reel"></ul>
+                </div>
+            </div>
+        </div>
 
-    <p id="token-counter">Tokens: 1000</p>
-    <p id="spin-result"></p>
-    <div id="tokens"></div>
-    <p id="total-payout">Total Payout: 0.00</p>
-    <table id="payouts-table">
-        <tr>
-            <th>Symbol</th>
-            <th>Type</th>
-            <th>Amount</th>
-        </tr>
-    </table>
+        <div class="text-center mt-5">
+            <button id="spin-button" class="btn btn-primary">Spin</button>
+        </div>
+
+        <div class="mt-5">
+            <h3>Winning Lines:</h3>
+            <ul id="winning-lines" class="list-group"></ul>
+        </div>
+
+        <div class="mt-5">
+            <h3>Tokens: <span id="token-amount">0</span></h3>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
     <script src="ajax.js"></script>
 </body>
-
 </html>
